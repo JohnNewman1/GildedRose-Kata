eb0bba50ecd7a33ee2cf5a4817c3202f50ffef32
George Sykes <georgesykes86@gmail.com>
2018-06-20 16:06:04 +0100

    Moves validation logic into increment and decrement methods in shop

--- src/shop.js
+++ src/shop.js
@@ -5,24 +5,16 @@ class Shop {
   updateQuality() {
     this.items.forEach((item) => {
       if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-        if (Shop._isNotZeroQuality(item)) {
-          if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-            Shop._decrementQuality(item);
-          }
-        }
+        Shop._decrementQuality(item);
       } else {
         if (Shop._isNotMaxQuality(item)) {
           Shop._incrementQuality(item);
           if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.sellIn < 11) {
-              if (Shop._isNotMaxQuality(item)) {
                 Shop._incrementQuality(item);
-              }
             }
             if (item.sellIn < 6) {
-              if (Shop._isNotMaxQuality(item)) {
-                Shop._incrementQuality(item);
-              }
+              Shop._incrementQuality(item);
             }
           }
         }
@@ -33,18 +25,12 @@ class Shop {
       if (item.sellIn < 0) {
         if (item.name !== 'Aged Brie') {
           if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
-            if (Shop._isNotZeroQuality(item)) {
-              if (item.name !== 'Sulfuras, Hand of Ragnaros') {
-                Shop._decrementQuality(item);
-              }
-            }
+            Shop._decrementQuality(item);
           } else {
             item.quality = item.quality - item.quality;
           }
         } else {
-          if (Shop._isNotMaxQuality(item)) {
-            Shop._incrementQuality(item);
-          }
+          Shop._incrementQuality(item);
         }
       }
     })
@@ -61,11 +47,17 @@ class Shop {
   }
 
   static _decrementQuality(item) {
-    item.quality -= 1;
+    if (Shop._isNotZeroQuality(item)) {
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.quality -= 1;
+      }
+    }
   }
 
   static _incrementQuality(item) {
-    item.quality += 1;
+    if (Shop._isNotMaxQuality(item)) {
+      item.quality += 1;
+    }
   }
 
   static _decrementSellIn(item) {
