bd9983b56d46a446f4b75df7d2361a7b260080ff
George Sykes <georgesykes86@gmail.com>
2018-06-20 17:48:27 +0100

    Creates new StandardItem class

---
+++ spec/unit/StandardItemSpec.js
@@ -0,0 +1,24 @@
+const { StandardItem } = require('../../src/standardItem');
+
+describe('StandardItem', () => {
+  let standardItem;
+  const name = 'Bread';
+  const sellIn = 10;
+  const quality = 5;
+
+  beforeEach(() => {
+    standardItem = new StandardItem(name, sellIn, quality);
+  });
+
+  it('returns the correct name', () => {
+    expect(standardItem.name).toEqual(name);
+  });
+
+  it('returns the correct name', () => {
+    expect(standardItem.sellIn).toEqual(sellIn);
+  });
+
+  it('returns the correct name', () => {
+    expect(standardItem.quality).toEqual(quality);
+  });
+});
---
+++ src/standardItem.js
@@ -0,0 +1,6 @@
+const { Item } = require('./item');
+
+class StandardItem extends Item {
+}
+
+module.exports.StandardItem = StandardItem;
