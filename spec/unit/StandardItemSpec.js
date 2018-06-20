69ed31e5f6d8845a33b2ec9d971528f3ea3b9af7
George Sykes <georgesykes86@gmail.com>
2018-06-20 17:56:32 +0100

    Adds updateSellIn method to StandardItem

--- spec/unit/StandardItemSpec.js
+++ spec/unit/StandardItemSpec.js
@@ -21,4 +21,17 @@ describe('StandardItem', () => {
   it('returns the correct name', () => {
     expect(standardItem.quality).toEqual(quality);
   });
+
+  it('decrements its sellIn property by one', () => {
+    standardItem.updateSellIn();
+
+    expect(standardItem.sellIn).toEqual(sellIn - 1);
+  });
+
+  it('decrements its sellIn property twice', () => {
+    standardItem.updateSellIn();
+    standardItem.updateSellIn();
+
+    expect(standardItem.sellIn).toEqual(sellIn - 2);
+  });
 });
--- src/standardItem.js
+++ src/standardItem.js
@@ -1,6 +1,9 @@
 const { Item } = require('./item');
 
 class StandardItem extends Item {
+  updateSellIn() {
+    this.sellIn -= 1;
+  }
 }
 
 module.exports.StandardItem = StandardItem;
