fdf7aebd05d8a8603a3fdeeddeb1fd4ea0fdd258
George Sykes <georgesykes86@gmail.com>
2018-06-20 11:35:55 +0100

    Adds unit tests for Item class

---
+++ spec/Unit/ItemSpec.js
@@ -0,0 +1,24 @@
+const { Item } = require('../../src/gilded_rose');
+
+describe('Item', () => {
+  let item;
+  const name = 'Bread';
+  const sellIn = 10;
+  const quality = 5;
+
+  beforeEach(() => {
+    item = new Item(name, sellIn, quality);
+  });
+
+  it('returns the correct name', () => {
+    expect(item.name).toEqual(name);
+  });
+
+  it('returns the correct name', () => {
+    expect(item.sellIn).toEqual(sellIn);
+  });
+
+  it('returns the correct name', () => {
+    expect(item.quality).toEqual(quality);
+  });
+});
