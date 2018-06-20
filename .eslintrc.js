f24253485894559b412f326d28192a7ba31b1340
George Sykes <georgesykes86@gmail.com>
2018-06-20 10:58:19 +0100

    Adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "plugins": ["jasmine"],
+    "extends": "airbnb-base",
+    "env": {
+      "jasmine": true
+  },
+    "rules": {
+        "no-underscore-dangle": 0,
+        "no-plusplus": ["error", {"allowForLoopAfterthoughts": true}]
+    }
+};
