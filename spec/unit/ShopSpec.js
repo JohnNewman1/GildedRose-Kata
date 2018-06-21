e98ab8ee378bb5a8033d3d4616b88f2aeb48ed1b
George Sykes <georgesykes86@gmail.com>
2018-06-21 14:47:50 +0100

    Adds unit tests for Shop with new functionality

--- spec/unit/ShopSpec.js
+++ spec/unit/ShopSpec.js
@@ -1,14 +1,25 @@
 const { Shop } = require('../../src/shop');
+const { StandardItem } = require('../../src/standardItem');
 
 describe('Shop', () => {
   let shop;
-  const items = ['An Item'];
+  const item = new StandardItem('name', 10, 10);
+  const item2 = new StandardItem('name', 10, 10);
+  const items = [item, item2];
 
   beforeEach(() => {
     shop = new Shop(items);
+    spyOn(item, 'updateProperties');
+    spyOn(item2, 'updateProperties');
+    shop.updateQuality();
   });
 
   it('returns the items', () => {
     expect(shop.items).toEqual(items);
+  });
+
+  it('calls updateProperties on items', () => {
+    expect(item.updateProperties).toHaveBeenCalled();
+    expect(item2.updateProperties).toHaveBeenCalled();
   });
 });
