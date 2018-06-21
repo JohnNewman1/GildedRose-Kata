914bf42ce2522880e2b63678bf9310fa07f714bf
George Sykes <georgesykes86@gmail.com>
2018-06-21 10:27:18 +0100

    Adds maximum quality check to MaturingItem

--- spec/unit/MaturingItemSpec.js
+++ spec/unit/MaturingItemSpec.js
@@ -4,7 +4,7 @@ describe('MaturingItem', () => {
   let maturingItem;
   const name = 'Aged Brie';
   const sellIn = 3;
-  const quality = 20;
+  const quality = 40;
 
   beforeEach(() => {
     maturingItem = new MaturingItem(name, sellIn, quality);
@@ -27,5 +27,18 @@ describe('MaturingItem', () => {
     for (let i = 0; i < 4; i++) { maturingItem.updateProperties(); }
 
     expect(maturingItem.quality).toEqual(quality + 7);
+  });
+
+  it('prevents quality increasing above 50', () => {
+    for (let i = 0; i < 6; i++) { maturingItem.updateProperties(); }
+
+    expect(maturingItem.quality).toEqual(50);
+  });
+
+  it('prevents quality increasing above 50 different start quality', () => {
+    const newMaturingItem = new MaturingItem(name, sellIn, 47);
+    for (let i = 0; i < 4; i++) { newMaturingItem.updateProperties(); }
+
+    expect(newMaturingItem.quality).toEqual(50);
   });
 });
--- src/maturingItem.js
+++ src/maturingItem.js
@@ -1,8 +1,18 @@
 const { StandardItem } = require('./standardItem');
 
 class MaturingItem extends StandardItem {
+  constructor(name, sellIn, quality) {
+    super(name, sellIn, quality);
+    this.MAX_QUALITY = 50;
+  }
+
   _updateQuality() {
     this.quality += (this._isPastSellIn()) ? this.NEW_QUALITY_CHANGE : this.QUALITY_CHANGE;
+    this._exceedsMaxQualityCheck();
+  }
+
+  _exceedsMaxQualityCheck() {
+    this.quality = (this.quality > this.MAX_QUALITY) ? this.MAX_QUALITY : this.quality;
   }
 }
 
