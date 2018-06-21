fd09e6181bc1863237fff1badfdb8e3de8956845
George Sykes <georgesykes86@gmail.com>
2018-06-21 11:04:33 +0100

    Adds update quality for increasing by 2 when 10 days to go

--- spec/unit/TicketItemSpec.js
+++ spec/unit/TicketItemSpec.js
@@ -3,7 +3,7 @@ const { TicketItem } = require('../../src/ticketItem');
 describe('TicketItem', () => {
   let ticketItem;
   const name = 'Backstage passes to a TAFKAL80ETC concert';
-  const sellIn = 15;
+  const sellIn = 12;
   const quality = 21;
 
   beforeEach(() => {
@@ -17,5 +17,14 @@ describe('TicketItem', () => {
 
   it('calls the updateProperties method on the super class', () => {
     expect(ticketItem.sellIn).toEqual(sellIn - 1);
+  });
+
+  it('increases in quality by one when sellIn > 10', () => {
+    expect(ticketItem.quality).toEqual(quality + 1);
+  });
+
+  it('increases in quality by two when sellIn <= 10', () => {
+    for (let i = 0; i < 2; i++) { ticketItem.updateProperties(); }
+    expect(ticketItem.quality).toEqual(quality + 4);
   });
 });
--- src/ticketItem.js
+++ src/ticketItem.js
@@ -1,7 +1,13 @@
 const { MaturingItem } = require('./maturingItem');
 
 class TicketItem extends MaturingItem {
+  _updateQuality() {
+    this.quality += (this._isTenDaysOrLess()) ? 2 : 1;
+  }
 
+  _isTenDaysOrLess() {
+    return this.sellIn <= 10;
+  }
 }
 
 module.exports.TicketItem = TicketItem;
