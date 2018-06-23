cc55c7b5b4319d139a1666bedc3fe904694da5f1
George Sykes <georgesykes86@gmail.com>
2018-06-23 12:16:22 +0100

    Implements Aged Brie strategy

--- spec/unit/QualityStrategySpec.js
+++ spec/unit/QualityStrategySpec.js
@@ -3,6 +3,7 @@ const { QualityStrategy } = require('../../src/qualityStrategy');
 describe('QualityStrategy', () => {
   let qualityStrategy;
   let fakeItem;
+  let pastSellInItem;
 
   beforeEach(() => {
     qualityStrategy = new QualityStrategy();
@@ -11,30 +12,82 @@ describe('QualityStrategy', () => {
       sellIn: 1,
       quality: 10,
     };
-    qualityStrategy.defaultStrategy(fakeItem);
-  });
-
-  it('has a default strategy that decrements the quality when sellIn > 0', () => {
-    expect(fakeItem.quality).toEqual(9);
-  });
-
-  it('has a default strategy that decrements the quality by 2 sellIn <= 0', () => {
-    const pastSellInItem = {
+    pastSellInItem = {
       name: 'any',
       sellIn: 0,
       quality: 10,
     };
-    qualityStrategy.defaultStrategy(pastSellInItem);
-    expect(pastSellInItem.quality).toEqual(8);
   });
 
-  it('has a default strategy that stops the quality going below 0', () => {
-    const pastSellInItem = {
-      name: 'any',
-      sellIn: 0,
-      quality: 1,
-    };
-    qualityStrategy.defaultStrategy(pastSellInItem);
-    expect(pastSellInItem.quality).toEqual(0);
+  describe('defaultStrategy', () => {
+    it('has a default strategy that decrements the quality when sellIn > 0', () => {
+      qualityStrategy.defaultStrategy(fakeItem);
+
+      expect(fakeItem.quality).toEqual(9);
+    });
+
+    it('has a default strategy that decrements the quality by 2 sellIn <= 0', () => {
+      qualityStrategy.defaultStrategy(pastSellInItem);
+
+      expect(pastSellInItem.quality).toEqual(8);
+    });
+
+    it('has a default strategy that stops the quality going below 0', () => {
+      const pastSellInItem2 = {
+        name: 'any',
+        sellIn: 0,
+        quality: 1,
+      };
+      qualityStrategy.defaultStrategy(pastSellInItem2);
+
+      expect(pastSellInItem2.quality).toEqual(0);
+    });
+
+    it('has a default strategy that stops the quality going below 0 from different start', () => {
+      const fakeItem2 = {
+        name: 'any',
+        sellIn: 1,
+        quality: 0,
+      };
+      qualityStrategy.defaultStrategy(fakeItem2);
+
+      expect(fakeItem2.quality).toEqual(0);
+    });
+  });
+
+  describe('agedBrieStrategy', () => {
+    it('has an Aged Brie strategy that increments the quality by one when sellIn > 0', () => {
+      qualityStrategy.agedBrieStrategy(fakeItem);
+
+      expect(fakeItem.quality).toEqual(11);
+    });
+
+    it('has an Aged Brie strategy that increments the quality by two when sellIn <= 0', () => {
+      qualityStrategy.agedBrieStrategy(pastSellInItem);
+
+      expect(pastSellInItem.quality).toEqual(12);
+    });
+
+    it('has an Aged Brie strategy that stops the quality going above 50', () => {
+      const pastSellInItem2 = {
+        name: 'any',
+        sellIn: 0,
+        quality: 49,
+      };
+      qualityStrategy.agedBrieStrategy(pastSellInItem2);
+
+      expect(pastSellInItem2.quality).toEqual(50);
+    });
+
+    it('has an Aged Brie strategy that stops the quality going above 50 from another start', () => {
+      const pastSellInItem2 = {
+        name: 'any',
+        sellIn: 1,
+        quality: 50,
+      };
+      qualityStrategy.agedBrieStrategy(pastSellInItem2);
+
+      expect(pastSellInItem2.quality).toEqual(50);
+    });
   });
 });
--- src/qualityStrategy.js
+++ src/qualityStrategy.js
@@ -7,16 +7,22 @@ class QualityStrategy {
       'Backstage passes to a TAFKAL80ETC concert': this.ticketStrategy,
       'Sulfuras, Hand of Ragnaros': this.sulfurasStrategy,
     };
-    this.QUALITY_INCREMENT = 1;
+    this.MAX_QUALITY = 50;
   }
 
   defaultStrategy(item) {
+    this.qualityIncrement = 1;
     this._incrementQuality(item);
     this._pastSellInCheck(item);
     this._checkQualityLimits(item);
   }
 
-  agedBrieStrategy() { }
+  agedBrieStrategy(item) {
+    this.qualityIncrement = -1;
+    this._incrementQuality(item);
+    this._pastSellInCheck(item);
+    this._checkQualityLimits(item);
+  }
 
   ticketStrategy() { }
 
@@ -26,10 +32,11 @@ class QualityStrategy {
     if (item.sellIn <= 0) { this._incrementQuality(item); }
   }
 
-  _incrementQuality(item) { item.quality -= this.QUALITY_INCREMENT; }
+  _incrementQuality(item) { item.quality -= this.qualityIncrement; }
 
   _checkQualityLimits(item) {
     if (item.quality < 0) { item.quality = 0; }
+    if (item.quality > this.MAX_QUALITY) { item.quality = this.MAX_QUALITY; }
   }
 }
 
